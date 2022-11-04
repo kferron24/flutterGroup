44db4604e1f24475d84a8189a538b23f3ef8b75c
Alexandre Baudry <alexandrebaudry@MacBook-Pro-de-Alexandre.local>
2022-11-04 11:37:19 +0100

    pull

--- flutter_application_1/lib/TextSlider.dart
+++ flutter_application_1/lib/TextSlider.dart
@@ -5,18 +5,18 @@ import 'package:flutter_application_1/LogPage.dart';
 import 'package:flutter_application_1/ProfileScreen.dart';
 
 import 'HomePage.dart';
-import 'TextSlider.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class TextSlider extends StatefulWidget {
+  const TextSlider({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<TextSlider> createState() => _TextSliderState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _TextSliderState extends State<TextSlider> {
   @override
   Widget build(BuildContext context) {
+    double _value = 20;
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -68,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const Text(
-                  "Home Screen",
+                  "Slider Question",
                   style: TextStyle(
                     color: Color.fromARGB(255, 0, 161, 172),
                     fontSize: 50.0,
@@ -77,7 +77,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 const SizedBox(height: 15.0),
                 const Text(
-                  "Do you want to test the",
+                  "How would you rate your",
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 25.0,
@@ -86,7 +86,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 const SizedBox(height: 15.0),
                 const Text(
-                  "Quizz",
+                  "experience out of 10",
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 25.0,
@@ -94,29 +94,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 const SizedBox(height: 22.0),
-                Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Color.fromRGBO(0, 53, 63, 1), width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))), //
-                  child: RawMaterialButton(
-                    fillColor: Color.fromRGBO(212, 111, 77, 1),
-                    elevation: 0.0,
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(17.0),
-                    ),
-                    onPressed: () async {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => const TextSlider()));
-                    },
-                    child: const Text("YES",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0,
-                        )),
-                  ),
+                Slider(
+                  min: 0.0,
+                  max: 100.0,
+                  value: _value,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value;
+                    });
+                  },
                 ),
                 const SizedBox(height: 12.0),
                 Container(
@@ -136,7 +122,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       Navigator.of(context).pushReplacement(MaterialPageRoute(
                           builder: (context) => const LogPage()));
                     },
-                    child: const Text("NO",
+                    child: const Text("SUBMIT",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 18.0,
