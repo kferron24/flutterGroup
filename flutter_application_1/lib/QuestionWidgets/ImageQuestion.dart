3512eefd8e1c9770dae568230d401bf4736eda7e
Ezzarzari <aezzarzari@enseirb-matmeca.fr>
2022-11-10 10:49:04 +0100

    Slider & RankedQuestion

--- flutter_application_1/lib/QuestionWidgets/ImageQuestion.dart
+++ flutter_application_1/lib/QuestionWidgets/ImageQuestion.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import '../Log/LogPage.dart';
 import '../Log/ProfileScreen.dart';
 import 'TextQuestion.dart';
