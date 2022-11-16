592885c12aee5a7f08256acd8b15dd0ad34973ea
Alexandre Baudry <alexandrebaudry@MacBook-Pro-de-Alexandre.local>
2022-11-16 09:38:30 +0100

    push

--- flutter_application_1/lib/QuestionClasses/dichotomic_class.dart
+++ flutter_application_1/lib/QuestionClasses/dichotomic_class.dart
@@ -2,29 +2,33 @@ import '../QuestionWidgets/dichotomic_question.dart';
 import 'question.dart';
 import 'package:flutter/material.dart';
 
-
 class DichotomicClass extends Question {
   List<String>? _options;
   List<int>? _next;
 
-  DichotomicClass(String txt, int identifiant, List<String> options, List<int> next) {
-    text=txt;
-    id=identifiant;
-    _options=options;
+  DichotomicClass(
+      String txt, int identifiant, List<String> options, List<int> next) {
+    text = txt;
+    id = identifiant;
+    _options = options;
     _next = next;
   }
 
   //  DichotomicClass(this.text,this.id,this._options,this._next);
 
-  
   factory DichotomicClass.fromJson(dynamic json) {
-    return DichotomicClass(json['text'] as String, json['id'] as int, json['options'] as List<String>, json['next'] as List<int> );
+    return DichotomicClass(json['text'] as String, json['id'] as int,
+        json['options'] as List<String>, json['next'] as List<int>);
   }
 
   @override
   StatefulWidget createWidget(List<Question> questions) {
     return DichotomicQuestion(
-        question: text, questionID: id!, options: _options!, next: _next!, listQuestions: questions);
+        question: text,
+        questionID: id!,
+        options: _options!,
+        next: _next!,
+        listQuestions: questions);
   }
 
   @override
